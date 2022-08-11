343d8a6a8e155b455f894537a239ed653209b270
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2022-08-11 15:51:42 -0300

    Seção 2 finalizada

---
+++ vendas/src/main/java/io/github/davi/Animal.java
@@ -0,0 +1,5 @@
+package io.github.davi;
+
+public interface Animal {
+    void fazerBarulho();
+}
--- vendas/src/main/java/io/github/davi/VendasApplication.java
+++ vendas/src/main/java/io/github/davi/VendasApplication.java
@@ -1,9 +1,10 @@
 package io.github.davi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
+
+    @Gato
+    private Animal animal;
+
+    @Bean
+    public CommandLineRunner executar(){
+            return args -> {
+              animal.fazerBarulho();
+            };
+    }
+
     @GetMapping("/hello")
     public String helloWorld(){
         return applicationName;
